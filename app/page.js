725925ecc005368451002f95208c9b2375c8ea96
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2024-08-01 19:13:20 +0600

    get firebase db data

--- app/page.js
+++ app/page.js
@@ -3,8 +3,31 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { firestore } from "@/firebase";
 import { Box, Typography } from "@mui/material";
+import { query, collection, getDocs } from "firebase/firestore";
 
 export default function Home() {
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState("");
+
+  const updateInventory = async () => {
+    //fetch the snapshot
+    const snapshot = query(collection(firestore, "inventory"));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      });
+    });
+    setInventory(inventoryList);
+  };
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
+
   return (
     <Box>
       <Typography variant="h1">Inventory Management</Typography>
