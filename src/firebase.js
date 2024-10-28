13190692c9ff0c0508870fd6f3c359a4462abf80
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2024-10-28 21:06:13 +0600

    connected firestore db

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBP_fk_eHVaQBoErK5GpwKO8qKYlBbU1sg",
+  authDomain: "inventory-management-1a2ed.firebaseapp.com",
+  projectId: "inventory-management-1a2ed",
+  storageBucket: "inventory-management-1a2ed.appspot.com",
+  messagingSenderId: "89233110704",
+  appId: "1:89233110704:web:99b95ecfc007a4882ced75",
+  measurementId: "G-MMXZTSBZKN"
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
+export { firestore };
