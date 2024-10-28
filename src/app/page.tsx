211c58f7fa84c1f77acc276a67474f993a73b684
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2024-10-28 22:52:26 +0600

    finish creating basic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,68 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="mx-10 md:mx-48 mt-10 md:mt-24 rounded-xl bg-green-200">
+      <h1 className="grid place-items-center text-2xl">Inventory Management</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/*Search bar */}
+      <form className="max-w-md mx-auto my-2">
+        <label htmlFor="inventorySearch" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+        <div className="relative">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+            </svg>
+          </div>
+          <input type="search" id="inventorySearch" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-opacity-10 bg-blue-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Search Inventory..." required />
+          <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 bg-opacity-50 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </form>
+
+      <div className="grid gap-4 p-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="p-4 bg-blue-100 rounded-lg flex justify-between items-center">
+          <span>Table</span>
+          <div className="flex space-x-2">
+            <button className="text-blue-500">+</button>
+            <button className="text-blue-500">-</button>
+          </div>
+        </div>
+        <div className="p-4 bg-blue-100 rounded-lg flex justify-between items-center">
+          <span>Table</span>
+          <div className="flex space-x-2">
+            <button className="text-blue-500">+</button>
+            <button className="text-blue-500">-</button>
+          </div>
+        </div>
+        <div className="p-4 bg-blue-100 rounded-lg flex justify-between items-center">
+          <span>Table</span>
+          <div className="flex space-x-2">
+            <button className="text-blue-500">+</button>
+            <button className="text-blue-500">-</button>
+          </div>
+        </div>
+        <div className="p-4 bg-blue-100 rounded-lg flex justify-between items-center">
+          <span>Table</span>
+          <div className="flex space-x-2">
+            <button className="text-blue-500">+</button>
+            <button className="text-blue-500">-</button>
+          </div>
+        </div>
+        <div className="p-4 bg-blue-100 rounded-lg flex justify-between items-center">
+          <span>Table</span>
+          <div className="flex space-x-2">
+            <button className="text-blue-500">+</button>
+            <button className="text-blue-500">-</button>
+          </div>
+        </div>
+        <div className="p-4 bg-blue-100 rounded-lg flex justify-between items-center">
+
+        </div>
+
+      </div>
+
+
+
     </div>
   );
 }
